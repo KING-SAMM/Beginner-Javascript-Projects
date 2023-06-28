2048b219f8a0f287a4e6112e17e2511e377bc0aa
kcsamm <kcsamm11@gmail.com>
2023-06-28 11:09:53 +0100

    SPA - Completed routing functionality

--- spa/frontend/static/js/index.js
+++ spa/frontend/static/js/index.js
@@ -14,10 +14,39 @@ const router = async () => {
         }
     });
 
-    console.log(potentialMatches);
+    // Get route that matches the url path
+    let match = potentialMatches.find(potentialMatche => potentialMatche.isMatch);
+
+    // Handle undefined routes - Default to dashboard
+    if(!match) {
+        match = {
+            isMatch: true,
+            route: routes[0]
+        }
+    }
+
+    console.log(match.route.view());
 }
 
-// Call the router function  w hen DOM loads
+// Control navigation using the history API
+const navigateTo = url => {
+    window.history.pushState(null, null, url);
+
+    router();
+}
+
+// Navigate to route when browser 'forward' and 'back' buttons are cliked
+window.addEventListener("popstate", router);
+
+// Call the router function  when DOM loads
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Navigate to route when nav link is clicked
+    document.body.addEventListener("click", (e) => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
     router();
 });
